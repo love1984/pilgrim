ecbbd7e9e620433bf87bf3b2bfeb28507cf161e3
Jerome Leclanche <adys@azura>
2010-06-20 20:25:01 +0300

    show: Define our own show()

--- show.py
+++ show.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sys
+import sys, os
 
 from pilgrim.BLPImagePlugin import BLPImageFile as BLP
 from pilgrim.FTEXImagePlugin import FTEXImageFile as FTEX
 
 usage = "Usage: %s image.{blp,ftc,...}" % (sys.argv[0])
+
+def show(img):
+	filename = "/tmp/DPdFJvcMX0VssrRHuv.png"
+	img.save(filename)
+	os.popen("eog %s" % (filename))
 
 def main():
 	try:
@@ -17,9 +22,9 @@ def main():
 	for f in args:
 		flower = f.lower()
 		if flower.endswith(".blp"):
-			BLP(f).show()
+			show(BLP(f))
 		elif flower.endswith(".ftc") or flower.endswith(".ftu"):
-			FTEX(f).show()
+			show(FTEX(f))
 		else:
 			print "Unknown file format for %s..." % (f)
 			print usage
