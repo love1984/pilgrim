79988cd73edf17e62ff695f3986d1d06db1d9176
Jerome Leclanche <adys@azura>
2010-09-15 23:53:33 +0100

    convert: Greatly simplify logic

--- convert.py
+++ convert.py
@@ -2,9 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import sys
-
-from pilgrim.BLPImagePlugin import BLPImageFile as BLP
-from pilgrim.FTEXImagePlugin import FTEXImageFile as FTEX
+from os.path import exists, splitext
+from pilgrim.utils import getDecoder
 
 usage = "Usage: %s image.{blp,ftc,...}" % (sys.argv[0])
 
@@ -13,21 +12,20 @@ def main():
 		args = sys.argv[1:]
 	except IndexError:
 		print usage
-		exit()
+		exit(1)
+	
 	for f in args:
-		print "Converting...", f
-		flower = f.lower()
-		if flower.endswith(".blp"):
-			try:
-				BLP(f).save(f+".png")
-			except IOError:
-				BLP(f).save(f+".jpg")
-		elif flower.endswith(".ftc") or flower.endswith(".ftu"):
-			FTEX(f).save(f+".png")
+		if not exists(f):
+			print "%r: No such file or directory" % (f)
+		
+		codec = getDecoder(f)
+		if codec:
+			print "Converting...", f
+			name, = splitext(f)
+			return codec(f).save(name + ".png")
 		else:
 			print "Unknown file format for %s..." % (f)
 			print usage
-	exit()
 
 if __name__ == "__main__":
 	main()
