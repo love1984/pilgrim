d4c89db67cc224967051de87811b0228e23b0ca7
Jerome Leclanche <adys@azura>
2011-02-20 09:29:43 +0000

    bin/show: Improve code quality

--- bin/show.py
+++ bin/show.py
@@ -1,34 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os.path
 import sys
-from os.path import exists, splitext
-from pilgrim.utils import getDecoder, show
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from pilgrim import utils
 
 usage = "Usage: %s image.{blp,ftc,...}" % (sys.argv[0])
 
 def main():
-	try:
-		args = sys.argv[1:]
-	except IndexError:
+	if len(sys.argv) < 2:
 		print usage
 		exit(1)
 	
 	files = []
-	for f in args:
-		if not exists(f):
+	for f in sys.argv[1:]:
+		if not os.path.exists(f):
 			print "%r: No such file or directory" % (f)
 		
-		codec = getDecoder(f)
+		codec = utils.getDecoder(f)
 		if codec:
 			print "Converting...", f
-			name, _ = splitext(f)
+			name, _ = os.path.splitext(f)
 			files.append(codec(f))
 		else:
 			print "Unknown file format for %s..." % (f)
 			print usage
 	
-	show(files)
+	utils.show(files)
 
 if __name__ == "__main__":
 	main()
