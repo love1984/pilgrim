ff975ba163b6b6b56151540cbdeae0735ca6ff68
Jerome Leclanche <adys@azura>
2011-02-20 12:11:55 +0000

    bin/show: Improve usage string

--- bin/show.py
+++ bin/show.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from pilgrim import utils
 
-usage = "Usage: %s image.{blp,ftc,...}" % (sys.argv[0])
+usage = "Usage: %s [FILE]..." % (sys.argv[0])
 
 def main():
 	if len(sys.argv) < 2:
